7d0ef8ff4a580b827da979cee34ee6ce47289ce0
Thirty33 <joelsuarez.1101@gmail.com>
2019-06-18 14:55:49 -0500

    enabling jason parsing to requests

--- config/web.php
+++ config/web.php
@@ -15,10 +15,10 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'QO2ZzNbUX9Xz6zZv9XC_eclJXa25FGuC',
-            'enableCsrfValidation' => false
-            // 'parsers' => [
-            //     'application/json' => 'yii\web\JsonParser',
-            // ]
+            'enableCsrfValidation' => false,
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
