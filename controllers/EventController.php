f1835e5e79246261887c41f6622b35e0cc3e2de8
Thirty33 <joelsuarez.1101@gmail.com>
2019-06-18 13:32:07 -0500

    api events crud

--- controllers/EventController.php
+++ controllers/EventController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace app\controllers;
-use Yii;
 use app\models\Event;
 
 
@@ -26,5 +25,41 @@ class EventController extends \yii\web\Controller
         else {
             return array('status' => false, 'data' => $event->getErrors());
         }
+    }
+
+    public function actionGetEvents() {
+        \Yii::$app->response->format = \yii\web\Response:: FORMAT_JSON;
+        $events = Event::find()->all();
+        if(count($events) > 0) {
+            return array('status' => true, 'data' => $events);
+        }
+        else {
+            return array('status' => false, 'data' => 'no events found');
+        }
+    }
+
+    public function actionUpdateEvent() {
+        \Yii::$app->response->format = \yii\web\Response:: FORMAT_JSON;
+        $attributes = \yii::$app->request->post();
+        $event = Event::find()->where(['event_id' => $attributes['id']])
+            ->one();
+        if(count($event) > 0) {
+            $event->attributes = \yii::$app->request->post();
+            $event->save();
+            return array('status' => true, 'data' => $event);
+        }
+        return array('status' => false, 'data' => 'event record unsuccesfully');
+    }
+
+    public function actionDeleteEvent() {
+        \Yii::$app->response->format = \yii\web\Response:: FORMAT_JSON;
+        $attributes = \yii::$app->request->post();
+        $event = Event::find()->where(['event_id' => $attributes['id']])
+            ->one();
+        if(count($event) > 0) {
+            $event->delete();
+            return array('status' => true, 'data' => 'event deleted');
+        }
+        return array('status' => false, 'data' => 'event not found');
     }
 }
